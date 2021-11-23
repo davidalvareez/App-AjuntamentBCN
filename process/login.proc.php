43bcfe3e2131279357daac627735f75fd1085d53
David <6694.joan23@fje.edu>
2021-11-23 17:05:28 +0100

    srguir

--- process/login.proc.php
+++ process/login.proc.php
@@ -1,3 +1,28 @@
 <?php
-//que haga lo que tnega que hacer
-header("Location:../view/listaeventos.php");
+require_once '../services/conexion.php';
+if (isset($_POST['email']) && isset($_POST['password'])) {
+    session_start();
+    require_once '../services/connection.php';
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $stmt = $pdo->prepare("SELECT * FROM tbl_perfil WHERE email='$email' and contraseña=MD5('{$password}')");
+    $stmt->execute();
+    $comprobacion=$stmt->fetchAll(PDO::FETCH_ASSOC);
+    try {
+            if (!empty($comprobacion)) {
+                foreach ($comprobacion as $row) {
+                    $_SESSION['nombre']=$row['nombre'];
+                    $_SESSION['tipo_user']=$row['tipo'];
+                 }   
+                //print_r($comprobacion);
+                $_SESSION['email']=$email;
+                header("location:../view/listaeventos.php");
+            }else {
+                header("location: ../view/login.php");
+            }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+}else{
+    header("location: ../view/login.php");
+}
--- process/register.proc.php
+++ process/register.proc.php
@@ -1,21 +1,25 @@
 <?php
+    if (isset($_POST['username']) && isset($_POST['password'])) {
     //falta hacer que si estan vacios los campos no haga todo esto
     include '../services/conexion.php';
     $user=$_POST['username'];
     $pass=$_POST['password'];
 
-    $consulta=$pdo->prepare("SELECT * FROM tbl_empleado WHERE email_emp = :email and pass_emp = :pass");  //preparas la consulta del elemento cuya existencia deseas validar   
+    $consulta=$pdo->prepare("SELECT * FROM tbl_perfil WHERE email = :email and pass = :pass");  //preparas la consulta del elemento cuya existencia deseas validar   
     $consulta->bindParam(':email', $user);
     $consulta->bindParam(':pass', $pass);
     $consulta->execute();
     $validaruser=$consulta->fetchColumn();
-
+    
     if ($validaruser) {
         //El usuario ya ha sido registrado
-        //enviar msg en el login o en el diciendo que esta actualizado
+        //enviar msg en el login o en el diciendo que esta registrado
         header("Location:../view/login.php");
     }else {
         //El usuario NO ha sido registrado
         //Insertar el usuario en la base de datos y redirigir a la pagina donde se ven eventos
         header("Location:../view/listaeventos.php");
-    }                              
+    }                             
+}else {
+    echo "xd";
+}
