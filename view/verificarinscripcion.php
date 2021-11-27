81f7a6db4167b47daea9b27f6c1c0fc7e1fda86b
Xavi <100006207.joan23@fje.edu>
2021-11-27 15:16:20 +0100

    eventos apuntados + sentencias SQL

---
+++ view/verificarinscripcion.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verificar DNI</title>
+</head>
+<body>
+    <h1>Verificar DNI</h1>
+    <form action="../process/verificardni.proc.php" method="post">
+        <input type="text" name="dni" id="dni">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
