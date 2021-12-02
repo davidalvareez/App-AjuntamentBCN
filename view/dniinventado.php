a9317107982e9260a85848ee67dceecb5167920e
David <6694.joan23@fje.edu>
2021-12-02 22:25:01 +0100

    falta boton dni

--- view/dniinventado.php
+++ view/dniinventado.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
     <title>Dni inventado</title>
 </head>
-<body>
-    
+<body class="login">
+    <div class="row flex-cv">
+        <div class="cuadro_dni_falso">
+            <h1 class="avisodni">El DNI no es valido, por favor, ingrese un DNI correcto.</h1>
+        </div>
+    </div>
 </body>
 </html>
